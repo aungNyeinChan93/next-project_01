434df98284df6bb82c2f18c4ad804315bc8cc054
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2025-08-12 14:37:54 +0630

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,23 @@
-"use client";
 import React from "react";
+import { Mogra } from "next/font/google";
+import Banner from "@/components/share/Banner";
+import Toggle from "@/components/share/Toggle";
 
+const mogra = Mogra({
+  weight: "400",
+  subsets: ["latin"],
+});
 const HomePage = () => {
   return (
     <React.Fragment>
-      <main className="pannel w-full min-h-screen ">
-        <h3>Home Page</h3>
-        <button
-          type="button"
-          onClick={() => document.documentElement.classList.toggle("dark")}
-          className="btn"
-        >
-          toggle
-        </button>
+      <main className="pannel w-full min-h-screen flex flex-col ">
+        <h3 className={`text-center text-3xl p-3  ${mogra.className}`}>
+          Home Page
+        </h3>
+        <section className="pannel flex-grow relative">
+          <Toggle />
+          <Banner />
+        </section>
       </main>
     </React.Fragment>
   );
